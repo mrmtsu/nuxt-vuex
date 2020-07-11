f4d154484a40e618294ca2a9620943ec5521acb7
mrmtsu <81202muramatsu@gmail.com>
2020-07-11 17:35:41 +0900

    モジュールの利用

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,20 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-vuex
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <!-- <p>{{ $store.state.message }}</p> -->
+      <p>{{ $store.state.hello.message }}</p>
+      <!-- <button @click="$store.commit('updateMessage')">Update</button> -->
+      <!-- <button @click="$store.commit('updateMessage', 'Commit with payload')">Update</button> -->
+      <!-- <button @click="$store.dispatch('updateMessageAction')">Dispatch</button> -->
+      <!-- <button @click="$store.dispatch('updateMessageAction', 'Dispatch with payload')">Dispatch</button> -->
+      <button
+        @click="$store.dispatch('hello/updateMessageAction', 'Dispatch with payload')"
+      >Dispatch</button>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
